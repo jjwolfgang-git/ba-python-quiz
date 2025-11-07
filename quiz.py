a88aa799fa9a2610c6afb4ff34700db615f1ed20
jjwolfgang-git <github@johnwolfgang.com>
2025-11-06 19:47:01 -0500

    Added comments describing question data structure

--- quiz.py
+++ quiz.py
@@ -1,5 +1,12 @@
 from quiz_questions import questions
 
+# Each question in the List is a Dictionary formatted as follows: 
+#  "question": "This is the question to display",
+#  "options": ["answer 1", "answer 2", "answer 3", "answer4"],
+#  "answer": "answer 2"
+# Note that there are some True/False questions - in other words, you
+# do not know how many potential answers each question has.
+
 def quiz():
     
     print("Welcome to the Python Quiz Game")
